85d4ca1ab58b98ebec5482f985c1835ab08a054d
elyaathlan1 <elyaathlan@gmail.com>
2025-09-10 12:57:23 +0300

    add island counter question

---
+++ Very_Hard/islandCounter.js
@@ -0,0 +1,26 @@
+/*
+Write an island counter for a 2D matrix. This function should return the number
+of distinct islands present.
+
+An island is a group of connected 1s (*vertically* or *horizontally* connected).
+0 represents water. Islands are distinct if they are separate connected groups.
+
+Examples
+
+islandCounter([
+  [1,1,0,0,0],
+  [1,1,0,1,1],
+  [0,0,0,1,1],
+  [0,0,0,0,0],
+  [1,1,0,0,1]
+]) ➞ 4
+
+islandCounter([
+  [0,0,0],
+  [0,0,0],
+  [0,0,0]
+]) ➞ 0
+*/
+
+function islandCounter(/* args */) {
+}
