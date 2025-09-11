87ee7f1bd217522f956687951c259597504bda3e
elyaathlan1 <elyaathlan@gmail.com>
2025-09-11 10:52:31 +0300

    valid

---
+++ Very_Hard/validParenthesis.js
@@ -0,0 +1,20 @@
+/*
+Write a validator for strings containing three types of parentheses: (, [, and {. 
+Return true if the string is regular (i.e., all open parentheses are closed in the right order).
+
+Examples
+
+regularParenthesis("{[()]}") ➞ true
+
+regularParenthesis("{[(])}") ➞ false
+
+regularParenthesis("()[]{}") ➞ true
+
+regularParenthesis("([)]") ➞ false
+
+regularParenthesis("") ➞ true
+*/
+function validParenthesis(/* args */) {
+}
+
+exports.solution = validParenthesis; 
